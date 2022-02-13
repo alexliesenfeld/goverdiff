7d2eb10b034f92ae8b1fb0996148ad4eae6efa17
Alexander Liesenfeld <alexander@hipeople.io>
2022-02-13 22:49:09 +0000

    Update

--- main.go
+++ main.go
@@ -109,10 +109,11 @@ func createOrUpdateComment(ctx context.Context, title, details string) {
 		panic(err)
 	}
 
-	linkMarkdown = ""
+	linkMarkdown := ""
 	if link != "" {
 		linkMarkdown = fmt.Sprintf("%s[Link](%s)\n", link)
 	}
+
 	// iterate over existing pull request comments - if existing coverage comment found then update
 	body := fmt.Sprintf("%s\n%s\n\n<details><summary>Details</summary>\n\n```\n%s```\n%s\n</details>\n",
 		coverageReportHeaderMarkdown,
